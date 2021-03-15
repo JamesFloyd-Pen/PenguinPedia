2e4d7aaa8d27af9acaea0a98d5647f4034bbbd68
JamesFloyd-Pen <Commanderpip@gmail.com>
2021-03-15 12:27:06 -0400

    Icon Change and basic layout

---
+++ app/src/main/java/com/example/penguinpedia/AutoPenguinGenerateActivity.java
@@ -0,0 +1,18 @@
+package com.example.penguinpedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AutoPenguinGenerateActivity extends AppCompatActivity {
+
+    /*
+    This file is use to generate a penguin when a user clicks X penguin's icon and arrive at this page.
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_auto_penguin_generate);
+    }
+}
---
+++ app/src/main/java/com/example/penguinpedia/Example_EmperorPenguin.java
@@ -0,0 +1,19 @@
+package com.example.penguinpedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Example_EmperorPenguin extends AppCompatActivity {
+    /*
+        This Emperor Penguin Activity is the example prototype to build the foundation
+        and layout for AutoPenguinGenerateActivity to build. This activity will be used
+        to build all necessary code for the view to function as intented.
+     */
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_example__emperor_penguin);
+    }
+}
