cabb959783a91f82004fe3e55cd438136444e6bb
JamesFloyd-Pen <Commanderpip@gmail.com>
2021-03-16 16:32:10 -0400

    Update Five
    Activites are now linked for a basic function. Will replace the button for an image Button with an emperor penguin

--- app/src/main/java/com/example/penguinpedia/MainActivity.java
+++ app/src/main/java/com/example/penguinpedia/MainActivity.java
@@ -2,19 +2,37 @@ package com.example.penguinpedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    //Emperor penguin building prototype
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn =  (Button)findViewById(R.id.EPbtn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent emperorPenguinIntent = new Intent(MainActivity.this, Example_EmperorPenguin.class);
+                startActivity(emperorPenguinIntent);
+            }
+
+
+        });
     }
+
 
     /*
     TODO
     1. Main code with 3 rows per penguin
     2. Make sure there is a holder for round buttons for 18 penguins
+    3. For Test Button, make sure to
      */
 }
