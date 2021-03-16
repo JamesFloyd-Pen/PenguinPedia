3ee2662fb006becd595f85f3c44d72997c3055ac
JamesFloyd-Pen <Commanderpip@gmail.com>
2021-03-16 15:05:59 -0400

    Update For
    First Prototype of the Mainscreen

--- app/src/main/java/com/example/penguinpedia/MainActivity.java
+++ app/src/main/java/com/example/penguinpedia/MainActivity.java
@@ -11,4 +11,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /*
+    TODO
+    1. Main code with 3 rows per penguin
+    2. Make sure there is a holder for round buttons for 18 penguins
+     */
 }
