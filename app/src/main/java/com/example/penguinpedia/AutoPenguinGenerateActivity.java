232fac8fe5b91a24c4357d9e2948bc42b0bb4343
JamesFloyd-Pen <Commanderpip@gmail.com>
2021-07-07 14:51:27 -0400

    AutoPenguin Example created. Will need to add placable values that'd change upon selection of X penguin

--- app/src/main/java/com/example/penguinpedia/AutoPenguinGenerateActivity.java
+++ app/src/main/java/com/example/penguinpedia/AutoPenguinGenerateActivity.java
@@ -10,6 +10,7 @@ public class AutoPenguinGenerateActivity extends AppCompatActivity {
     This file is use to generate a penguin when a user clicks X penguin's icon and arrive at this page.
      */
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
